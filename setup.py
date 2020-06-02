a84ea7057bbf7ac61e7e7ffcbcba77a3cd5a7191
methylDragon <methylDragon@gmail.com>
2020-06-02 21:29:29 +0800

    Set up package

---
+++ setup.py
@@ -0,0 +1,101 @@
+import os
+import re
+import sys
+import platform
+import subprocess
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
+
+__version__ = '0.0.1'
+package_name = "rmf_mock_adapter_python"
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the following"
+                               "extensions: "
+                               + ", ".join(e.name for e in self.extensions))
+
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)',
+                                         out.decode()).group(1))
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(
+            os.path.dirname(self.get_ext_fullpath(ext.name))
+            )
+        # required for auto-detection of auxiliary "native" libs
+        if not extdir.endswith(os.path.sep):
+            extdir += os.path.sep
+
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += [
+                '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'
+                .format(cfg.upper(), extdir)
+            ]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j2']
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'\
+            .format(env.get('CXXFLAGS', ''), self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
+                              cwd=self.build_temp,
+                              env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args,
+                              cwd=self.build_temp)
+
+
+setup(
+    name=package_name,
+    version=__version__,
+    packages=[], #['wrap_add_test'],
+    #package_dir={'wrap_add_test': 'scripts/wrap_add_test'},
+    author='methylDragon',
+    author_email='methylDragon@gmail.com',
+    url='',
+    description='Python bindings for rmf_mock_adapter',
+    long_description='',
+    ext_modules=[CMakeExtension(package_name)],
+    setup_requires=['pybind11>=2.5.0'],
+    cmdclass={'build_ext': CMakeBuild},
+    zip_safe=False,
+    entry_points={'console_scripts': [
+        #'wrap_add_test = wrap_add_test.wrap_add_test:main'
+    ]},
+    license='Apache License, Version 2.0',
+    install_requires=['setuptools'],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml'])
+    ]
+)
