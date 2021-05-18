604abe42dcbdc9535ba3ad3f257fea1f14187eb1
Yadunund <yadunund@openrobotics.org>
2021-05-18 13:29:39 +0800

    Fix test failure due to charger waypoint
    Signed-off-by: Yadunund <yadunund@openrobotics.org>

--- rmf_fleet_adapter/test/phases/MockAdapterFixture.cpp
+++ rmf_fleet_adapter/test/phases/MockAdapterFixture.cpp
@@ -41,7 +41,7 @@ MockAdapterFixture::MockAdapterFixture()
   ros_node = adapter->node();
 
   const std::string test_map_name = "test_map";
-  graph.add_waypoint(test_map_name, {0.0, -10.0}); // 0
+  graph.add_waypoint(test_map_name, {0.0, -10.0}).set_charger(true); // 0
   graph.add_waypoint(test_map_name, {0.0, -5.0});  // 1
   graph.add_waypoint(test_map_name, {5.0, -5.0}).set_holding_point(true);  // 2
   graph.add_waypoint(test_map_name, {-10.0, 0.0}); // 3
