c34055822c299eec9571850cbf8027b59a5be2c6
Michael X. Grey <grey@openrobotics.org>
2021-12-29 15:40:37 +0800

    Remove redundant comparison function
    Signed-off-by: Michael X. Grey <grey@openrobotics.org>

--- rmf_traffic_ros2/test/unit/test_ParticipantRegistry.cpp
+++ rmf_traffic_ros2/test/unit/test_ParticipantRegistry.cpp
@@ -49,28 +49,6 @@ SCENARIO("Test idempotency of shape type")
   REQUIRE_THROWS(shape_type(node["type"]));
 }
 
-namespace rmf_traffic {
-
-bool operator==(const Profile p1, const Profile p2)
-{
-  return rmf_traffic_ros2::convert(p1) == rmf_traffic_ros2::convert(p2);
-}
-
-namespace schedule {
-
-bool operator==(
-  const ParticipantDescription desc1,
-  const ParticipantDescription desc2)
-{
-  return desc1.name() == desc2.name()
-    && desc1.owner() == desc2.owner()
-    && desc1.responsiveness() ==  desc2.responsiveness()
-    && desc1.profile() == desc2.profile();
-}
-
-}
-}
-
 namespace rmf_traffic_ros2 {
 namespace schedule {
 
