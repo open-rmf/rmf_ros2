3e52711b5e99583b40146b8a96f4210868524af5
Morgan Quigley <morgan@osrfoundation.org>
2022-02-13 11:49:03 +0800

    Support for geojson graphs (#142)
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>
    Signed-off-by: Morgan Quigley <morgan@osrfoundation.org>
    Signed-off-by: Michael X. Grey <grey@openrobotics.org>
    Co-authored-by: Morgan Quigley <morgan@osrfoundation.org>
    Co-authored-by: Luca Della Vedova <luca@openrobotics.org>
    Co-authored-by: Michael X. Grey <grey@openrobotics.org>

---
+++ rmf_traffic_ros2/include/rmf_traffic_ros2/agv/Graph.hpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2021 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <rmf_traffic/agv/Graph.hpp>
+
+#include <rmf_site_map_msgs/msg/site_map.hpp>
+
+namespace rmf_traffic_ros2 {
+
+//==============================================================================
+rmf_traffic::agv::Graph convert(const rmf_site_map_msgs::msg::SiteMap& from,
+  int graph_idx = 0, double wp_tolerance = 1e-3);
+
+} // namespace rmf_traffic_ros2
