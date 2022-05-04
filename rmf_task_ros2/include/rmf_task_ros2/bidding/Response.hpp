3fb323d17de5c7baba59e22a0731d827ed426c3d
abilash <abilash.sivalingam95@gmail.com>
2022-05-04 14:12:49 +0800

    successful build

--- rmf_task_ros2/include/rmf_task_ros2/bidding/Response.hpp
+++ rmf_task_ros2/include/rmf_task_ros2/bidding/Response.hpp
@@ -22,6 +22,7 @@
 #include <rmf_task_msgs/msg/bid_notice.hpp>
 #include <rmf_task_msgs/msg/bid_proposal.hpp>
 #include <rmf_task_msgs/msg/bid_response.hpp>
+#include <optional>
 
 namespace rmf_task_ros2 {
 namespace bidding {
