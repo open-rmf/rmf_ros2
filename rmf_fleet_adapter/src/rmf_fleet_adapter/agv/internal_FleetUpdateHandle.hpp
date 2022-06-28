4e9bee77e4343163dc927624c79ff56cc50b511b
Yadunund <yadunund@openrobotics.org>
2022-06-28 19:38:58 +0800

    Implement speed limit API and publish lane_states
    Signed-off-by: Yadunund <yadunund@openrobotics.org>

--- rmf_fleet_adapter/src/rmf_fleet_adapter/agv/internal_FleetUpdateHandle.hpp
+++ rmf_fleet_adapter/src/rmf_fleet_adapter/agv/internal_FleetUpdateHandle.hpp
@@ -36,6 +36,7 @@
 #include <rmf_task_sequence/events/PerformAction.hpp>
 
 #include <rmf_fleet_msgs/msg/dock_summary.hpp>
+#include <rmf_fleet_msgs/msg/lane_states.hpp>
 
 #include <rmf_fleet_adapter/agv/FleetUpdateHandle.hpp>
 #include <rmf_fleet_adapter/StandardNames.hpp>
@@ -312,6 +313,11 @@ public:
 
   mutable rmf_task::Log::Reader log_reader = {};
 
+  using LaneStates = rmf_fleet_msgs::msg::LaneStates;
+  rclcpp::Publisher<LaneStates>::SharedPtr lane_states_pub = nullptr;
+  std::unordered_map<std::size_t, double> speed_limited_lanes = {};
+  std::unordered_set<std::size_t> closed_lanes = {};
+
   template<typename... Args>
   static std::shared_ptr<FleetUpdateHandle> make(Args&& ... args)
   {
@@ -379,6 +385,13 @@ public:
         if (const auto self = w.lock())
           self->_pimpl->dock_summary_cb(msg);
       });
+
+    // Publish LaneStates
+    handle->_pimpl->lane_states_pub =
+      handle->_pimpl->node->create_publisher<LaneStates>(
+      LaneStatesTopicName,
+      transient_qos);
+    handle->_pimpl->publish_lane_states();
 
     // Populate charging waypoints
     const auto& graph = (*handle->_pimpl->planner)->get_configuration().graph();
@@ -534,6 +547,8 @@ public:
   }
 
   void publish_fleet_state_topic() const;
+
+  void publish_lane_states() const;
 
   void update_fleet() const;
 
