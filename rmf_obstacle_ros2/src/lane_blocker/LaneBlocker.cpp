53117169f3f6872638d44d8386caa2e0315abf02
Yadunund <yadunund@openrobotics.org>
2022-06-29 20:16:19 +0800

    Compute transfroms to rmf frame
    Signed-off-by: Yadunund <yadunund@openrobotics.org>

--- rmf_obstacle_ros2/src/lane_blocker/LaneBlocker.cpp
+++ rmf_obstacle_ros2/src/lane_blocker/LaneBlocker.cpp
@@ -18,7 +18,14 @@
 
 #include "LaneBlocker.hpp"
 
+#include <rmf_traffic_ros2/agv/Graph.hpp>
 #include <rmf_fleet_adapter/StandardNames.hpp>
+
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/vector3_stamped.hpp>
+
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 #include <rclcpp_components/register_node_macro.hpp>
 
@@ -26,56 +33,151 @@
 LaneBlocker::LaneBlocker(const rclcpp::NodeOptions& options)
 : Node("lane_blocker_node", options)
 {
-  _data = std::make_shared<Data>();
 
-  _data->lane_closure_pub = this->create_publisher<LaneRequest>(
+  _tf2_buffer =
+    std::make_unique<tf2_ros::Buffer>(this->get_clock());
+
+  if (_tf2_buffer != nullptr)
+  {
+    _transform_listener =
+      std::make_shared<tf2_ros::TransformListener>(*_tf2_buffer);
+  }
+
+  _rmf_frame = this->declare_parameter("rmf_frame_id", "map");
+  RCLCPP_INFO(
+    this->get_logger(),
+    "Setting parameter rmf_frame_id to %s", _rmf_frame.c_str()
+  );
+
+  _obstacle_lane_threshold = this->declare_parameter(
+    "obstacle_lane_threshold", 0.5);
+  RCLCPP_INFO(
+    this->get_logger(),
+    "Setting parameter obstacle_lane_threshold to %f", _obstacle_lane_threshold
+  );
+
+  _tf2_lookup_duration = this->declare_parameter(
+    "tf2_lookup_duration", 0.5);
+  RCLCPP_INFO(
+    this->get_logger(),
+    "Setting parameter tf2_lookup_duration to %f", _tf2_lookup_duration
+  );
+
+  _lane_closure_pub = this->create_publisher<LaneRequest>(
     rmf_fleet_adapter::LaneClosureRequestTopicName,
     rclcpp::SystemDefaultsQoS());
 
-  _data->obstacle_sub = this->create_subscription<Obstacles>(
+  _speed_limit_pub = this->create_publisher<SpeedLimitRequest>(
+    rmf_fleet_adapter::SpeedLimitRequestTopicName,
+    rclcpp::SystemDefaultsQoS());
+
+  _obstacle_sub = this->create_subscription<Obstacles>(
     "rmf_obstacles",
-    rclcpp::SystemDefaultsQoS(),
-    [data = _data](const Obstacles& msg)
+    rclcpp::QoS(10).best_effort(),
+    [=](Obstacles::ConstSharedPtr msg)
     {
-      data->obstacle_cb(msg);
+      obstacle_cb(*msg);
     });
 
-  auto transient_qos = rclcpp::SystemDefaultsQoS().reliable().transient_local();
+  // Selectively disable intra-process comms for non-volatile subscriptions
+  // so that this node can be run in a container with intra-process comms.
+  auto transient_qos = rclcpp::QoS(10).transient_local();
+  rclcpp::SubscriptionOptionsWithAllocator<
+    std::allocator<void>> ipc_sub_options;
+  ipc_sub_options.use_intra_process_comm =
+    rclcpp::IntraProcessSetting::Disable;
 
-  _data->graph_sub = this->create_subscription<NavGraph>(
+  _graph_sub = this->create_subscription<NavGraph>(
     "nav_graphs",
     transient_qos,
-    [data = _data](std::shared_ptr<const NavGraph> msg)
+    [=](NavGraph::ConstSharedPtr msg)
     {
-      data->graph_cb(std::move(msg));
-    });
+      if (msg->name.empty())
+        return;
+      auto traffic_graph = rmf_traffic_ros2::convert(*msg);
+      if (!traffic_graph.has_value())
+      {
+        RCLCPP_WARN(
+          this->get_logger(),
+          "Unable to convert NavGraph from fleet %s into a traffic graph",
+          msg->name.c_str()
+        );
+      }
+      _traffic_graphs[msg->name] = std::move(traffic_graph.value());
+    },
+    ipc_sub_options);
 
-  _data->lane_states_sub = this->create_subscription<LaneStates>(
+  _lane_states_sub = this->create_subscription<LaneStates>(
     "lane_states",
-    rclcpp::SystemDefaultsQoS(),
-    [data = _data](std::shared_ptr<const LaneStates> msg)
+    transient_qos,
+    [=](LaneStates::ConstSharedPtr msg)
     {
-      data->lane_states_cb(std::move(msg));
-    });
+      if (msg->fleet_name.empty())
+        return;
+      _lane_states[msg->fleet_name] = msg;
+    },
+    ipc_sub_options);
 
 }
 
 //==============================================================================
-void LaneBlocker::Data::obstacle_cb(const Obstacles& msg)
+void LaneBlocker::obstacle_cb(const Obstacles& msg)
 {
+  using PoseStamped = geometry_msgs::msg::PoseStamped;
+  using Vector3Stamped = geometry_msgs::msg::Vector3Stamped;
 
-}
+  if (msg.obstacles.empty() || _transform_listener == nullptr)
+    return;
 
-//==============================================================================
-void LaneBlocker::Data::graph_cb(std::shared_ptr<const NavGraph> msg)
-{
+  // TODO(YV): Consider using tf2_ros::MessageFilter instead of this callback
+  for (const auto& obstacle : msg.obstacles)
+  {
+    const auto& obstacle_frame = obstacle.header.frame_id;
+    std::string tf2_error;
+    const bool can_transform = _tf2_buffer->canTransform(
+      _rmf_frame,
+      obstacle_frame,
+      tf2::TimePointZero,
+      tf2::durationFromSec(_tf2_lookup_duration),
+      &tf2_error);
 
-}
+    // TODO(YV): Cache these transforms since most of them would be static
+    if (!can_transform)
+    {
+      RCLCPP_WARN(
+        this->get_logger(),
+        "Unable to lookup transform between between obstacle frame %s and RMF "
+        "frame %s.", obstacle_frame.c_str(), _rmf_frame.c_str()
+      );
+      continue;
+    }
 
-//==============================================================================
-void LaneBlocker::Data::lane_states_cb(std::shared_ptr<const LaneStates> msg)
-{
+    const auto& transform = _tf2_buffer->lookupTransform(
+      _rmf_frame,
+      obstacle_frame,
+      tf2::TimePointZero
+    );
 
+    // doTransform only works on Stamped messages
+    const auto before_pose = geometry_msgs::build<PoseStamped>()
+      .header(obstacle.header)
+      .pose(obstacle.bbox.center);
+    const auto before_size = geometry_msgs::build<Vector3Stamped>()
+      .header(obstacle.header)
+      .vector(obstacle.bbox.size);
+
+    PoseStamped after_pose;
+    Vector3Stamped after_size;
+    tf2::doTransform(before_pose, after_pose, transform);
+    tf2::doTransform(before_size, after_size, transform);
+    RCLCPP_INFO(
+      this->get_logger(),
+      "Pose of obstacle id %d in RMF %s frame is [%f, %f, %f]",
+      obstacle.id, _rmf_frame.c_str(),
+      after_pose.pose.position.x,
+      after_pose.pose.position.y, after_pose.pose.position.z
+    );
+  }
 }
 
 RCLCPP_COMPONENTS_REGISTER_NODE(LaneBlocker)
--- rmf_obstacle_ros2/src/lane_blocker/LaneBlocker.hpp
+++ rmf_obstacle_ros2/src/lane_blocker/LaneBlocker.hpp
@@ -20,10 +20,18 @@
 
 #include <rclcpp/rclcpp.hpp>
 
+#include <rmf_traffic/agv/Graph.hpp>
+
 #include <rmf_obstacle_msgs/msg/obstacles.hpp>
 #include <rmf_building_map_msgs/msg/graph.hpp>
 #include <rmf_fleet_msgs/msg/lane_request.hpp>
+#include <rmf_fleet_msgs/msg/speed_limit_request.hpp>
 #include <rmf_fleet_msgs/msg/lane_states.hpp>
+
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+
+#include <unordered_map>
 
 //==============================================================================
 /// Modify states of lanes for fleet adapters based on density of obstacles
@@ -32,7 +40,9 @@ class LaneBlocker : public rclcpp::Node
 public:
   using Obstacles = rmf_obstacle_msgs::msg::Obstacles;
   using NavGraph = rmf_building_map_msgs::msg::Graph;
+  using TrafficGraph = rmf_traffic::agv::Graph;
   using LaneRequest = rmf_fleet_msgs::msg::LaneRequest;
+  using SpeedLimitRequest = rmf_fleet_msgs::msg::SpeedLimitRequest;
   using LaneStates = rmf_fleet_msgs::msg::LaneStates;
 
   /// Constructor
@@ -40,20 +50,22 @@ public:
     const rclcpp::NodeOptions& options  = rclcpp::NodeOptions());
 
 private:
-  struct Data
-  {
     void obstacle_cb(const Obstacles& msg);
-    void graph_cb(std::shared_ptr<const NavGraph> msg);
-    void lane_states_cb(std::shared_ptr<const LaneStates> msg);
 
-    rclcpp::Subscription<Obstacles>::SharedPtr obstacle_sub;
-    rclcpp::Subscription<NavGraph>::SharedPtr graph_sub;
-    rclcpp::Subscription<LaneStates>::SharedPtr lane_states_sub;
-    rclcpp::Publisher<LaneRequest>::SharedPtr lane_closure_pub;
-  };
+    rclcpp::Subscription<Obstacles>::SharedPtr _obstacle_sub;
+    rclcpp::Subscription<NavGraph>::SharedPtr _graph_sub;
+    rclcpp::Subscription<LaneStates>::SharedPtr _lane_states_sub;
+    rclcpp::Publisher<LaneRequest>::SharedPtr _lane_closure_pub;
+    rclcpp::Publisher<SpeedLimitRequest>::SharedPtr _speed_limit_pub;
+    double _tf2_lookup_duration;
 
-  std::shared_ptr<Data> _data;
+    std::string _rmf_frame;
+    std::unique_ptr<tf2_ros::Buffer> _tf2_buffer;
+    std::shared_ptr<tf2_ros::TransformListener> _transform_listener;
 
+    std::unordered_map<std::string, TrafficGraph> _traffic_graphs;
+    std::unordered_map<std::string, LaneStates::ConstSharedPtr> _lane_states;
+    double _obstacle_lane_threshold;
 };
 
 
