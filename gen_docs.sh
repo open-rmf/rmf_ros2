c6e18a1c0e8f95287e41c895ab4788f6211e3c91
orensbruli <orensbruli@users.noreply.github.com>
2022-12-10 19:16:02 +0000

    deploy: 14b7f855cc0717caa1f6a60c5540e1e249f603bd

---
+++ gen_docs.sh
@@ -0,0 +1,10 @@
+pydoc3 -w rmf_adapter \
+          rmf_adapter.geometry \
+          rmf_adapter.type \
+          rmf_adapter.plan \
+          rmf_adapter.vehicletraits \
+          rmf_adapter.graph \
+          rmf_adapter.nodes \
+          rmf_adapter.easy_traffic_light \
+          rmf_adapter.battery \
+          rmf_adapter.schedule
