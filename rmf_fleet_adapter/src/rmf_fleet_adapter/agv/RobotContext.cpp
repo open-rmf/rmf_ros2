adf7fcf90817e260e945d38557bb8f4d9230da7c
Grey <mxgrey@intrinsic.ai>
2025-05-03 12:50:16 +0800

    Reduce churn in lift supervisor (#418)
    Signed-off-by: Michael X. Grey <mxgrey@intrinsic.ai>

--- rmf_fleet_adapter/src/rmf_fleet_adapter/agv/RobotContext.cpp
+++ rmf_fleet_adapter/src/rmf_fleet_adapter/agv/RobotContext.cpp
@@ -1626,7 +1626,10 @@ void RobotContext::_check_lift_state(
       state.session_id.c_str());
   }
 
-  _publish_lift_destination();
+  if (_lift_destination && _lift_destination->lift_name == state.lift_name)
+  {
+    _publish_lift_destination();
+  }
 }
 
 //==============================================================================
