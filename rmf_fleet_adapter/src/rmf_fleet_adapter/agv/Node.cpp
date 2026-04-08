34deb8afff294a290a8ee415ceb3f67271267d26
Aaron Chong <aaronchongth@gmail.com>
2026-04-08 16:05:27 +0800

    revert string literal
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- rmf_fleet_adapter/src/rmf_fleet_adapter/agv/Node.cpp
+++ rmf_fleet_adapter/src/rmf_fleet_adapter/agv/Node.cpp
@@ -72,7 +72,7 @@ std::shared_ptr<Node> Node::make(
 
   node->_emergency_notice_obs =
     node->create_observable<EmergencyNotice>(
-    "fire_alarm_trigger", transient_qos);
+    rmf_traffic_ros2::EmergencyTopicName, transient_qos);
 
   node->_target_emergency_notice_obs =
     node->create_observable<TargetEmergencyNotice>(
